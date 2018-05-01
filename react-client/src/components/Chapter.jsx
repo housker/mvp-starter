56e7f93fa7102bd17c6ca3f948b6f6332672b490
Adelle Housker <housker@gmail.com>
2018-04-30 23:27:45 -0500

    able to get diff between oldContent and newContent

--- react-client/src/components/Chapter.jsx
+++ react-client/src/components/Chapter.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+// import createRef from 'create-react-ref';
+import ReactQuill from 'react-quill';
+// import 'react-quill/dist/quill.snow.css';
+// import ReactDOM from 'react-dom';
 import Quill from 'quill';
 import Delta from 'quill-delta';
 
@@ -7,41 +11,84 @@ class Chapter extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      // oldQuill: {}
+      // api: '',
+      // user: ''
     }
+    this.quillRef = null;
+    this.reactQuillRef = null;
+    // this.oldContent = {};
+    this.change = new Delta();
+    this._handleChange = this._handleChange.bind(this);
+    this._handleChangeSelection = this._handleChangeSelection.bind(this);
+    // this.handleClick = this.handleClick.bind(this);
+    // this.props.attachQuillRefs.bind(this);
+    // React.createRef();
+    // this.handleChange = this.handleChange.bind(this)
   }
 
   componentDidMount() {
-
+    // console.log('This in componentDidMount: ', this)
+    // if(this.props.mode === 'editor') {
+    //   this.props.attachQuillRefs()
+    // }
   }
 
   componentDidUpdate() {
-    console.log("Chapter mounting")
-    if(this.props.mode === 'editor') {
-      var options = {
-        debug: 'infxo',
-        modules: {
-          clipboard: {
-            matchVisual: true
-          },
-          toolbar: '#toolbar'
-        }
-      };
-      var editor = new Quill('.editor-container', options);
-      var change = new Delta();
-      editor.on('text-change', function(delta) {
-        change = change.compose(delta);
-        console.log(change)
-      });
+    if(this.quillRef === null && this.props.mode === 'editor' && typeof this.reactQuillRef.getEditor === 'function') {
+      this.quillRef = this.reactQuillRef.getEditor();
+      this.props.loadQR(this.quillRef)
+      // this.oldContent = this.quillRef.getContents();
     }
   }
 
+  // attachQuillRefs() {
+  //   // console.log('this inside attachQuillRefs: ', this)
+  //   if (typeof this.reactQuillRef.getEditor !== 'function') return;
+  //   this.quillRef = this.reactQuillRef.getEditor();
+  //   this.props.loadQR(this.quillRef)
+  //   console.log('this.quillRef: ', this.quillRef)
+  //   // console.log('this.quillRef.getContents(): ', this.quillRef.getContents())
+  // }
+
+  _handleChange(value, delta, source, editor) {
+    // console.log('this: ', this)
+    // console.log('value: ', value)
+    // console.log('delta: ', delta)
+    // console.log('source: ', source)
+    // var myDelta = new Delta();
+    // console.log('this.reactQuillRef: ', this.reactQuillRef)
+    // this.reactQuillRef.on('text-change', delta => {
+    //   myDelta = myDelta.compose(delta);
+    //   console.log('myDelta: ', myDelta);
+    // })
+
+    // this.change = this.change.compose(delta)
+    // console.log('this.change', this.change)
+    // console.log('editor.getContents: ', editor.getContents())
+    // console.log('this: ', this.refs.editor.focus())
+    // if (source == 'user') {
+    //   var accumulator = []
+    //   accumulator.push(delta)
+    //   console.log("Accumulator: ", accumulator)
+    // }
+  }
+
+  _handleChangeSelection(range, source, editor) {
+    // console.log('Arguments of _handleChangeSelection: ', arguments)
+
+  }
+
+  // accumulate() {
+
+  // }
+
   render() {
+    const mode = this.props.mode === 'editor' ? ( <ReactQuill ref={(el) => { this.reactQuillRef = el }} theme="bubble" value={this.props.content} onChange={this._handleChange} onChangeSelection={this._handleChangeSelection} /> ) : ( <div>{this.props.content}</div> );
     return (
       <div>
         <h2>{this.props.title}</h2>
-        <div className="editor-container">
-        <div>{this.props.content}</div>
-        </div>
+        {mode}
       </div>
     )
   }
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import Quill from 'quill';
+import ReactQuill from 'react-quill';
 import List from './components/List.jsx';
 import Chapter from './components/Chapter.jsx';
 import Editor from './components/Editor.jsx';
@@ -10,24 +11,39 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      quillRef: {},
       globeMode: false,
       mode: 'chapter',
       title: 'Boulder, CO',
       content: 'Lots of words are here.'
     }
-    this.edit = this.edit.bind(this);
+    this.quillRef = null;
+    this.oldContent = {};
+    // this.reactQuillRef = null;
+    // this.myRef = React.createRef();
+    // this.attachQuillRefs = this.attachQuillRefs.bind(this);
+    this.loadQR = this.loadQR.bind(this)
+    this.click = this.click.bind(this);
   }
 
   componentDidMount() {
+    // this.attachQuillRefs()
   }
 
 
   componentDidUpdate() {
+    // this.attachQuillRefs()
   // console.log("My component is updating")
   //   if(this.state.mode === 'editor') {
   //     var editor = new Quill('.editor');
   //   }
   }
+
+  // attachQuillRefs() {
+  //   if (typeof this.reactQuillRef.getEditor !== 'function') return;
+  //   this.quillRef = this.reactQuillRef.getEditor();
+  // }
+
 
     // $.ajax({
     //   url: '/items',
@@ -41,9 +57,60 @@ class App extends React.Component {
     //   }
     // });
 
-  edit() {
-    this.setState({mode: 'editor'})
-    console.log('Edit clicked!')
+  // attachQuillRefs() {
+  //   console.log('this inside attachQuillRefs: ', this)
+  //   // if (typeof this.reactQuillRef.getEditor !== 'function') return;
+  //   // this.quillRef = this.reactQuillRef.getEditor();
+  // }
+
+  loadQR(qr) {
+    // console.log('qr: ', qr)
+    this.quillRef = qr;
+    this.oldContent = this.quillRef.getContents();
+    console.log('this.oldContent: ', this.oldContent)
+    // console.log('this.quillRef: ', this.quillRef)
+    // this.setState({quillRef: qr})
+    // console.log('this.state.quillRef: ', this.state.quillRef)
+  }
+
+  save() {
+    var newContent = this.quillRef.getContents()
+    var diff = this.oldContent.diff(newContent)
+    console.log('diff: ', diff)
+
+
+
+    // this.quillRef.on('editor-change', function(eventName, ...args) {
+    //   if (eventName === 'text-change') {
+    //     console.log("I AM INSIDE TEXT_CHANGE")
+    //     // args[0] will be delta
+    //   } else if (eventName === 'selection-change') {
+    //     // args[0] will be old range
+    //   }
+    // });
+  }
+
+  click() {
+    switch (this.state.mode) {
+      case 'globe':
+        this.setState({mode: 'chapter'});
+        break;
+      case 'chapter':
+        this.setState({mode: 'editor'});
+        break;
+      case 'editor':
+        this.save();
+        this.setState({mode: 'chapter'});
+        // console.log("Clicked inside editor! ", this)
+        // this.forceUpdate();
+        break;
+      default:
+      console.log('mode not recognized')
+    }
+
+    // if(this.state.mode === )
+    // this.setState({mode: 'editor'});
+    // console.log('Edit clicked!')
     // this.forceUpdate();
     // var editor = new Quill('.editor');
 
@@ -65,10 +132,10 @@ class App extends React.Component {
       console.log('mode not recognized')
     }
     // let button = this.state.mode ? 'pull request' : 'fork';
-  const mode = this.state.globeMode ? ( <Globe title={this.state.title} content={this.state.content} button={button}/> ) : ( <Chapter mode={this.state.mode} title={this.state.title} content={this.state.content} edit={this.edit} button={button}/>);
+  const mode = this.state.globeMode ? ( <Globe title={this.state.title} content={this.state.content} button={button}/> ) : ( <Chapter mode={this.state.mode} title={this.state.title} content={this.state.content} edit={this.edit} loadQR={this.loadQR} button={button}/> );
     return (<div>
       {mode}
-      <button onClick={this.edit}>{button}</button>
+      <button onClick={this.click}>{button}</button>
     </div>)
   }
 }
