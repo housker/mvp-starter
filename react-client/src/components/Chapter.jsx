38258f6d0e1fdfdc6cc483a2eebda746df452886
Adelle Housker <housker@gmail.com>
2018-04-30 23:33:47 -0500

    cleaned up code

--- react-client/src/components/Chapter.jsx
+++ react-client/src/components/Chapter.jsx
@@ -11,76 +11,27 @@ class Chapter extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      // oldQuill: {}
-      // api: '',
-      // user: ''
     }
     this.quillRef = null;
     this.reactQuillRef = null;
-    // this.oldContent = {};
-    this.change = new Delta();
-    this._handleChange = this._handleChange.bind(this);
-    this._handleChangeSelection = this._handleChangeSelection.bind(this);
-    // this.handleClick = this.handleClick.bind(this);
-    // this.props.attachQuillRefs.bind(this);
-    // React.createRef();
-    // this.handleChange = this.handleChange.bind(this)
+    // this._handleChange = this._handleChange.bind(this);
+    // this._handleChangeSelection = this._handleChangeSelection.bind(this);
   }
 
   componentDidMount() {
-    // console.log('This in componentDidMount: ', this)
-    // if(this.props.mode === 'editor') {
-    //   this.props.attachQuillRefs()
-    // }
   }
 
   componentDidUpdate() {
     if(this.quillRef === null && this.props.mode === 'editor' && typeof this.reactQuillRef.getEditor === 'function') {
       this.quillRef = this.reactQuillRef.getEditor();
       this.props.loadQR(this.quillRef)
-      // this.oldContent = this.quillRef.getContents();
     }
   }
 
-  // attachQuillRefs() {
-  //   // console.log('this inside attachQuillRefs: ', this)
-  //   if (typeof this.reactQuillRef.getEditor !== 'function') return;
-  //   this.quillRef = this.reactQuillRef.getEditor();
-  //   this.props.loadQR(this.quillRef)
-  //   console.log('this.quillRef: ', this.quillRef)
-  //   // console.log('this.quillRef.getContents(): ', this.quillRef.getContents())
+  // _handleChange(value, delta, source, editor) {
   // }
 
-  _handleChange(value, delta, source, editor) {
-    // console.log('this: ', this)
-    // console.log('value: ', value)
-    // console.log('delta: ', delta)
-    // console.log('source: ', source)
-    // var myDelta = new Delta();
-    // console.log('this.reactQuillRef: ', this.reactQuillRef)
-    // this.reactQuillRef.on('text-change', delta => {
-    //   myDelta = myDelta.compose(delta);
-    //   console.log('myDelta: ', myDelta);
-    // })
-
-    // this.change = this.change.compose(delta)
-    // console.log('this.change', this.change)
-    // console.log('editor.getContents: ', editor.getContents())
-    // console.log('this: ', this.refs.editor.focus())
-    // if (source == 'user') {
-    //   var accumulator = []
-    //   accumulator.push(delta)
-    //   console.log("Accumulator: ", accumulator)
-    // }
-  }
-
-  _handleChangeSelection(range, source, editor) {
-    // console.log('Arguments of _handleChangeSelection: ', arguments)
-
-  }
-
-  // accumulate() {
-
+  // _handleChangeSelection(range, source, editor) {
   // }
 
   render() {
